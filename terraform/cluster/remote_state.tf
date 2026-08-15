0c16e48cb5722404fb5eb3491c52368ae2af9fd9
sudd22 <seudali200@gmail.com>
2026-08-15 22:57:15 +0100

    cluster

--- terraform/cluster/remote_state.tf
+++ terraform/cluster/remote_state.tf
@@ -1,7 +1,7 @@
 data "terraform_remote_state" "persistent" {
   backend = "s3"
   config = {
-    bucket = "b2b-eks-platform-tfstate-boutique"
+    bucket = "microservice-eks-platform-tfstate-boutique"
     key    = "persistent/terraform.tfstate"
     region = "eu-west-2"
   }
