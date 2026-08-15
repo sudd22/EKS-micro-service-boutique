32c0664d1e46d67c303a6eafd1f5378da3358e25
sudd22 <seudali200@gmail.com>
2026-08-15 22:56:48 +0100

    cluster

--- terraform/cluster/providers.tf
+++ terraform/cluster/providers.tf
@@ -1,19 +1,32 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = var.region
-    default_tags {
-        tags = {
-            Project     = "b2b-eks-platform"
-            Environment = "multi-tenant-cluster"
-            ManagedBy   = "terraform"   
-        }
+  region = var.region
+  default_tags {
+    tags = {
+      Project     = "microservice-eks-platform"
+      Environment = "multi-tenant-cluster"
+      ManagedBy   = "terraform"
     }
+  }
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project     = "microservice-eks-platform"
+      Environment = "multi-tenant-cluster"
+      ManagedBy   = "terraform"
+    }
+  }
 }
