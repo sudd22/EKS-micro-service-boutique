d93d1d29bfe48576841adab741c16594e0d1b645
sudd22 <seudali200@gmail.com>
2026-08-31 23:34:24 +0100

    test

---
+++ app/conftest.py
@@ -0,0 +1,6 @@
+import os
+os.environ.setdefault("DB_USER", "postgres")
+os.environ.setdefault("DB_PASSWORD", "postgres")
+os.environ.setdefault("DB_HOST", "localhost")
+os.environ.setdefault("DB_NAME", "microservice")
+os.environ.setdefault("DB_SCHEMA", "public")
